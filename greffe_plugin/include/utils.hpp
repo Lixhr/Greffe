011c407804a96cd0598da9ab392c0278bb09ba70
Charlie <>
2026-04-12 20:23:11 +0200

    init bg colors

--- greffe_plugin/include/utils.hpp
+++ greffe_plugin/include/utils.hpp
@@ -1,3 +1,15 @@
 #include "kernwin.hpp"
+#include "ida.hpp"
 
 #define greffe_msg(fmt, ...) msg("[greffe] " fmt, ##__VA_ARGS__)
+
+namespace Color {
+    constexpr bgcolor_t RELOCATED = 0xff3b0e32;
+    constexpr bgcolor_t PATCHED = 0xff852a4c;
+    constexpr bgcolor_t PATCH_REGION = 0xff8A84E2;
+    constexpr bgcolor_t TARGET = RELOCATED;
+}
+
+void set_range_color(ea_t start, ea_t end, bgcolor_t color);
+
+void clear_all_range_colors();
---
+++ greffe_plugin/src/GUI/BGColor.cpp
@@ -0,0 +1,66 @@
+#include <ida.hpp>
+#include <idp.hpp>
+#include <kernwin.hpp>
+#include "utils.hpp"
+#include <vector>
+#include <algorithm>
+
+struct ColoredRange {
+    ea_t      start;
+    ea_t      end;
+    bgcolor_t color;
+};
+
+static std::vector<ColoredRange> s_ranges;
+
+struct RangeColorHooks : public event_listener_t {
+    ssize_t idaapi on_event(ssize_t code, va_list va) override {
+        if (code == processor_t::ev_get_bg_color) {
+            bgcolor_t *color = va_arg(va, bgcolor_t *);
+            ea_t       ea    = va_arg(va, ea_t);
+            for (const auto& r : s_ranges) {
+                if (ea >= r.start && ea < r.end) {
+                    *color = r.color;
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+};
+
+extern plugin_t PLUGIN;
+
+static RangeColorHooks s_hooks;
+static bool            s_hooked = false;
+
+void set_range_color(ea_t start, ea_t end, bgcolor_t color)
+{
+    s_ranges.erase(
+        std::remove_if(s_ranges.begin(), s_ranges.end(),
+            [&](const ColoredRange& r) { return r.start == start && r.end == end; }),
+        s_ranges.end());
+
+    if (color != DEFCOLOR)
+        s_ranges.push_back({start, end, color});
+
+    if (!s_ranges.empty() && !s_hooked) {
+        hook_event_listener(HT_IDP, &s_hooks, &PLUGIN);
+        s_hooked = true;
+    } else if (s_ranges.empty() && s_hooked) {
+        unhook_event_listener(HT_IDP, &s_hooks);
+        s_hooked = false;
+    }
+
+    request_refresh(IWID_DISASM);
+}
+
+void clear_all_range_colors()
+{
+    s_ranges.clear();
+    if (s_hooked) {
+        unhook_event_listener(HT_IDP, &s_hooks);
+        s_hooked = false;
+    }
+    request_refresh(IWID_DISASM);
+}
