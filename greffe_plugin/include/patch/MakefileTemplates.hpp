48f888bf6ec4b431166288961f4a0b533f5503e6
Charlie <>
2026-04-16 09:09:07 +0200

    force Makefile rebuild

--- greffe_plugin/include/patch/MakefileTemplates.hpp
+++ greffe_plugin/include/patch/MakefileTemplates.hpp
@@ -30,33 +30,7 @@ build/%.o: handlers/%.c | build
 build:
 	mkdir -p build
 
-.PHONY: all
-)makefile" },
-
-    { "arm64", R"makefile(CC      := aarch64-none-elf-gcc
-OBJCOPY := aarch64-none-elf-objcopy
-CFLAGS  := -fno-pic -nostdlib -Os -ffunction-sections -fdata-sections -Ttext=0x0
-
-NON_GREFFE_SRCS := $(filter-out %_greffe.c,$(wildcard handlers/*.c))
--include greffe_active.mk
-SRCS := $(NON_GREFFE_SRCS) $(ACTIVE_GREFFE_SRCS)
-OBJS := $(patsubst handlers/%.c,build/%.o,$(SRCS))
-
-all: build/handlers.bin build/handlers.elf
-
-build/handlers.elf: $(OBJS) | build
-	$(CC) $(CFLAGS) -Wl,-Map=build/handlers.map -o $@ $^
-
-build/handlers.bin: build/handlers.elf | build
-	$(OBJCOPY) -O binary $< $@
-
-build/%.o: handlers/%.c | build
-	$(CC) $(CFLAGS) -c $< -o $@
-
-build:
-	mkdir -p build
-
-.PHONY: all
+.PHONY: all $(OBJS) build/handlers.elf build/handlers.bin
 )makefile" },
 };
 
